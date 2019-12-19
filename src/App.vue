afcd95f0709f7e41012544258d39464b3ea2e429
pse17 <fqibymeksm@protonmail.com>
2019-12-19 16:57:31 +0500

    sdfsdfsdf

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <header>
+      <Navbar/>
+    </header>
+      <Sidebar msg=""/>
+      <div class="content">
+        <h2>H2 content</h2>
+      </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sidebar,
+    Navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+  font-size: 16px;
+  background-color:#fff;
 }
+
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="sidebar">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
